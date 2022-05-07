76df23a6c39b3b8aef5c4cfd01aae84821fb0195
Shams Nahid <b.m.shamsnahid@gmail.com>
2022-05-08 00:24:51 +0600

    managed nat client

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 
 import { app } from '../../app';
 import { Ticket } from '../../models/ticket';
+import { natsWrapper } from '../../nats-wrapper';
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
   const response = await request(app).post('/api/tickets').send({});
@@ -77,4 +78,19 @@ it('creates ticket with valid parameters', async () => {
   expect(tickets.length).toEqual(1);
   expect(tickets[0].price).toEqual(10);
   expect(tickets[0].title).toEqual(title);
+});
+
+it('publishes an event', async () => {
+  const title = 'Jay Z Live Concert';
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title,
+      price: 10
+    })
+    .expect(201);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import mongoose from 'mongoose';
 
 import { app } from '../../app';
+import { natsWrapper } from '../../nats-wrapper';
 
 it('returns a 404 if the provided id does not exist', async () => {
   const id = new mongoose.Types.ObjectId().toHexString();
@@ -104,4 +105,28 @@ it('update the ticket with valid inputs', async () => {
 
   expect(ticketResponse.body.title).toEqual('Updated Concert Name');
   expect(ticketResponse.body.price).toEqual(100);
+});
+
+it('publishes an event', async () => {
+  const cookie = global.signin();
+
+  // create a ticket
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', cookie)
+    .send({
+      title: 'asdasd',
+      price: 20
+    });
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'Updated Concert Name',
+      price: 100
+    })
+    .expect(200);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
